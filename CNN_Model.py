b987b80f703f1fd537e9b1f979c8d61a1fb3d5c1
Ethan707 <1056183440@qq.com>
2021-03-04 13:44:07 +0800

    Implement custom data set class

---
+++ CNN_Model.py
@@ -0,0 +1,62 @@
+import numpy as np
+import os
+import cv2
+import matplotlib.pyplot as plt
+from pathlib import Path
+import random
+from PIL import Image
+from CustomDataset import CustomDataset
+from torchvision import transforms
+import FunctionTest
+
+# Load the data from the Google Drive
+# data_dir = Path("/content/drive/MyDrive/Data")
+
+# path of data set for local
+data_dir = Path("./dataset")
+
+# images: the list contain the path of each images
+images = list(data_dir.glob("*.jpg"))
+print("Number of images found: ", len(images))
+
+# ******************Sample part*****************
+# # Some sample in the data
+# sample_images = images[:4]
+# _, ax = plt.subplots(2, 2, figsize=(5, 3))
+
+# image = cv2.imread(str(sample_images[0]))
+# print("image shape", image.shape)
+
+# for i in range(4):
+#     img = cv2.imread(str(sample_images[i]))
+#     # print("Shape of image: ", img.shape)
+#     ax[i // 2, i % 2].imshow(img)
+#     ax[i // 2, i % 2].axis('off')
+# plt.show()
+# ****************Sample End********************
+
+# **********************TEST PASS***************
+# # Test Function for the data set class
+# FunctionTest.test_dataset_class(images)
+
+# **********************Test END***************
+
+random.shuffle(images)
+print("Dataset size:", len(images))
+# Split the data set
+
+# test data
+test_data = images[8000:]  # 2000 for test
+
+# the part for training
+training = images[:8000]
+valid_data = training[6000:]  # 2000 for validation
+train_data = training[:6000]  # 6000 for train
+
+print("test set size:", len(test_data))
+print("validation set size:", len(valid_data))
+print("train set size:", len(train_data))
+
+train_set = CustomDataset(train_data, transform=transforms.ToTensor)
+valid_set = CustomDataset(valid_data, transform=transforms.ToTensor)
+test_set = CustomDataset(test_data, transform=transforms.ToTensor)
