50dd359c59b553f97cd3538ae4a56a3b77e9a62c
ZjTan4 <mk12ss@outlook.com>
2021-03-09 18:51:51 -0700

    RNN implemented

--- RNN.py
+++ RNN.py
@@ -7,27 +7,59 @@ from torch.autograd import Variable
 
 N_DIGITS = 6
 
-class RNN(nn.Module):
+class Encoder(nn.Module):
     def __init__(self):
-        super(RNN, self).__init__()
-
-        self.rnn = nn.LSTM(
-            input_size=50,
-            hidden_size=128,
-            num_layers=1, 
-            batch_first=True,
+        super(Encoder, self).__init__()
+        self.fc1 = nn.Sequential(
+            nn.Linear(150, 128),
+            nn.BatchNorm1d(num_features=128),
+            nn.ReLU()
         )
-        
-        self.out = nn.Linear(128, 36 * 6)
+        self.lstm = nn.LSTM(input_size=200*128//5,
+                            hidden_size=256,
+                            num_layers=2,
+                            batch_first=True)
 
     def forward(self, x):
-        # input x in the form of (batch_size, 1, height, width)
-        # reformat it to (batch_size, time_steps=width, input_size=height)
-        x = x.permute(0, 1, 3, 2)
-        x = x.reshape(100, 200, 50)
-        
-        r_out, (h_n, h_c) = self.rnn(x, None) 
+        # (128, 3, 200, 50) -> (128, 150, 200)
+        x = x.reshape(-1, 150, 200).permute(0, 2, 1)
+        x = x.reshape(-1, 150)
+        fc1 = self.fc1(x)
+        fc1 = fc1.reshape(-1, 5, 200*128//5)
+        lstm, (h_n, h_c) = self.lstm(fc1, None)
+        out = lstm[:, -1, :]
 
-        # choose r_out at the last time step
-        out = self.out(r_out[:, -1, :])
         return out
+
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+        self.lstm = nn.LSTM(input_size=256,
+                            hidden_size=128,
+                            num_layers=2,
+                            batch_first=True)
+        self.out = nn.Linear(6*128, 6 * 36)
+
+    def forward(self, x):
+        # (N,256) -> (N,6,256) -> (N,6,128) -> (N, 6*128) -> (N,6* 36) -> (N,6,36)
+        x = x.reshape(-1, 1, 256) 
+        x = x.expand(-1, 6, 256)
+        lstm, (h_n, h_c) = self.lstm(x, None)
+        y1 = lstm.reshape(-1, 6*128)
+        out = self.out(y1)
+        output = out.reshape(-1, 6, 36)
+        return output
+
+
+class RNN (nn.Module):
+    def __init__(self):
+        super(RNN, self).__init__()
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+
+    def forward(self, x):
+        encoder = self.encoder(x)
+        decoder = self.decoder(encoder)
+
+        return decoder
--- RNN_train.py
+++ RNN_train.py
@@ -1,87 +1,149 @@
-from torch.optim import optimizer
-from CustomDataset import CustomDataset
+# The pure version of the CNN model
+import numpy as np
+import os
+import cv2
+import matplotlib.pyplot as plt
 from pathlib import Path
 import random
-from torchvision import transforms
+from PIL import Image
 import torch
+from RNN_CustomDataset import CustomDataset
+from CustomDataset import vector_to_captcha
+from torchvision import transforms
 from torch.utils.data import DataLoader
-from RNN import RNN   
+from CNN_Model import CNN
+from RNN import RNN 
+import torch.nn as nn
 
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(torch.cuda.get_device_name(0))
 
 # Load the data from the Google Drive
 # data_dir = Path("/content/drive/MyDrive/Data")
 
 # path of data set for local
-dataDir = Path("./archive")
+data_dir = Path("./archive")
 
 # images: the list contain the path of each images
-images = list(dataDir.glob("*.jpg"))
+images = list(data_dir.glob("*.jpg"))
+print("Number of images found: ", len(images))
+
 random.shuffle(images)
+print("Dataset size:", len(images))
+# Split the data set
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print(torch.cuda.get_device_name(0))
-
-# single validation
-testData = images[8000:]  # 2000 for test
+# test data
+test_data = images[8000:]  # 2000 for test
 
 # the part for training
 training = images[:8000]
-validData = training[6000:]  # 2000 for validation
-trainData = training[:6000]  # 6000 for train
-BATCH_SIZE = 100
+valid_data = training[6000:]  # 2000 for validation
+train_data = training[:6000]  # 6000 for train
 
-# init database
-testSet = CustomDataset(testData, transform=transforms.ToTensor())
-trainSet = CustomDataset(trainData, transform=transforms.ToTensor())
-validSet = CustomDataset(validData, transform=transforms.ToTensor())
+print("test set size:", len(test_data))
+print("validation set size:", len(valid_data))
+print("train set size:", len(train_data))
 
-trainDataloader = DataLoader(dataset=trainSet, batch_size=BATCH_SIZE, shuffle=True)
-testDataloader = DataLoader(dataset=testSet, batch_size=BATCH_SIZE, shuffle=True)
-validDataloader = DataLoader(dataset=validSet, batch_size=BATCH_SIZE, shuffle=True)
+train_set = CustomDataset(train_data)
+valid_set = CustomDataset(valid_data)
+test_set = CustomDataset(test_data)
 
-IMAGE_SIZE = trainSet.height * trainSet.width
+train_dataloader = DataLoader(dataset=train_set, batch_size=100, shuffle=True)
+test_dataloader = DataLoader(dataset=test_set, batch_size=1, shuffle=True)
+valid_dataloader = DataLoader(dataset=valid_set, batch_size=1, shuffle=True)
+
 model = RNN().to(device)
-criterion = torch.nn.MultiLabelSoftMarginLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
-def train(model, trainLoader, validLoader, device):  
-    model.train() 
-    for epoch in range(1000):
+
+def train(model, train_dataloader, valid_dataloader, device):
+    model.train()
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    for epoch in range(100):
         model.train()
-        for _, (images, labels) in enumerate(trainLoader):
-            images = images.to(device, dtype=torch.float32)
-            labels = labels.to(device, dtype=torch.float32)
-            
-            #predict = model(images, None)[0]
+        for i, (images, labels) in enumerate(train_dataloader):
+            images = images.to(device)
+            labels = labels.to(device)
+
             predict = model(images)
             optimizer.zero_grad()
-            
             loss = criterion(predict, labels)
             loss.backward()
             optimizer.step()
+        accuracy = valid(model, valid_dataloader, device)
+        print("epoch: {} loss: {:.10f} accuracy: {:.4f}".format(
+            (epoch+1), loss.item(), accuracy))
 
-        if (epoch + 1) % 5 == 0:
-            accuracy = valid(model, validLoader, device)
-            print("epoch: {} loss: {:.10f} accuracy: {:.4f}".format(
-                (epoch+1), loss.item(), accuracy))
 
-def valid(model, validLoader, device):
+def valid(model, valid_dataloader, device):
     num_correct = 0  # the counter for the correct items
-    num_total = len(validLoader)*100  # the counter for the total items
-    mean_acc = 0  # the accuracy of the validation
+    num_total = len(valid_dataloader)  # the counter for the total items
     model.eval()  # set the evaluation state of the model
     with torch.no_grad():
-        for _, (images, labels) in enumerate(validLoader):
+        for _, (images, labels) in enumerate(valid_dataloader):
             images = images.to(device)
             labels = labels.to(device)
-            #output = model(images, None)
             output = model(images)
-            labels = labels.reshape((100, 6, 36))
-            output = output.reshape((100, 6, 36))
-            labels = torch.argmax(labels, dim=2)
-            output = torch.argmin(output, dim=2)
-            num_correct += ((output == labels).sum(dim=1) == 6).sum().item()
-        mean_acc = num_correct / num_total * 100
-        return mean_acc
+            labels = labels.reshape(6, 36)
+            output = output.reshape(6, 36)
+            # get the captcha character index
+            labels = torch.argmax(labels, dim=1)
+            # get the predict character index
+            output = torch.argmax(output, dim=1)
+            num_correct += ((labels == output).sum() == 6).sum().item()
+        accuracy = num_correct / num_total * 100
+        return accuracy
 
-train(model, trainDataloader, testDataloader, device) 
+
+def test(model, test_dataloader, device):
+    num_correct = 0  # the counter for the correct items
+    num_total = len(test_dataloader)  # the counter for the total items
+    model.eval()  # set the evaluation state of the model
+
+    with torch.no_grad():
+        for _, (images, labels) in enumerate(test_dataloader):
+            images = images.to(device)
+            labels = labels.to(device)
+            output = model(images)
+            labels = labels.reshape(6, 36)
+            output = output.reshape(6, 36)
+            # get the captcha character index
+            labels = torch.argmax(labels, dim=1)
+            # get the predict character index
+            output = torch.argmax(output, dim=1)
+            num_correct += ((labels == output).sum() == 6).sum().item()
+        accuracy = num_correct / num_total * 100
+        return accuracy
+
+
+print("\nTraining")
+train(model, train_dataloader, valid_dataloader, device)
+
+print("\nTesting")
+accuracy = test(model, test_dataloader, device)
+print("Accuracy: {}".format(accuracy))
+
+# Sample
+
+model.eval()  # set the evaluation state of the model
+_, ax = plt.subplots(2, 3, figsize=(30, 15))
+with torch.no_grad():
+    for i in range(6):
+        image, label = next(iter(test_dataloader))
+
+        image = image.to(device)
+        label = label.to(device)
+        output = model(image)
+        label = label.reshape(6, 36)
+        output = output.reshape(6, 36)
+        label = torch.argmax(label, dim=1)
+        output = torch.argmax(output, dim=1)
+        origin = vector_to_captcha(label)
+        predict = vector_to_captcha(output)
+
+        image = image.reshape(50, 200)
+        image = image.cpu()
+        ax[i//5, i % 5].imshow(image)
+        ax[i//5, i % 5].title.set_text("Origin: "+origin+" Predict: "+predict)
+plt.show()
